5adf445015ed0526d58fb26e2cdef4e2c901cd16
Avi Mehta <avimehta2003@gmail.com>
2025-02-08 17:34:23 -0800

    step 1

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,37 @@
 // projects.js
-
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 import { fetchJSON, renderProjects } from '../global.js';
+let data = [1, 2, 3, 4, 5, 5];
+
+// Compute total sum of data
+let total = data.reduce((sum, d) => sum + d, 0);
+
+// Compute start and end angles for each slice
+let angle = 0;
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+
+// Define colors
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+// Select the SVG container
+let svg = d3.select("#projects-plot");
+
+// Define arc generator
+let arcGenerator = d3.arc()
+    .innerRadius(0)
+    .outerRadius(50);
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+// Append paths dynamically based on arcData
+arcData.forEach((d, idx) => {
+    svg.append("path")
+        .attr("d", arcGenerator(d))
+        .attr("fill", colors(idx))
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+});
 
 async function loadProjects() {
     // Fetch project data from projects.json
